f804a9cad5d5b9652a10c257ba23bc07920f248a
grotius-cnc <44880102+grotius-cnc@users.noreply.github.com>
2022-02-04 17:48:38 -0500

    Update main.cpp
    easy now?

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@ using namespace std;
 
     int main()
     {
-        dxfRW dxf("/tmp/file.dxf");
+        dxfRW dxf("file.dxf");
+        //! Subclassing. Not doing other stuff.
         ExampleWriter writer(dxf);
+
+        //! Fill up the subclassed "writer" with data.
+        DRW_Line line;
+        line.basePoint.x = 10.20;
+        line.basePoint.y = 20.10;
+        line.secPoint.x = 30.25;
+        line.secPoint.y = 30;
+        writer.linevec.push_back(line);
+
+        //! Here it will write all data at once to the dxf file.
         dxf.write(&writer, DRW::Version::AC1027, false);
         return 0;
     }
